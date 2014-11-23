457c462937b4e4de63d976288fbbe416f9315291
anmart <andrewrmartinek@gmail.com>
2014-11-23 06:19:14 -0500

    login stuff update
    asdfasdfasdfafsd

--- collab/moreinformation.php
+++ collab/moreinformation.php
@@ -1,35 +1,39 @@
-<DOCTYPE1 html>
+<!DOCTYPE html>
 <html>
-    <head>
-		<link rel="stylesheet" type="text/css" media="screen" href="main.css">
-		<link rel="stylesheet" type="text/css" media="only screen and (max-width: 480px), only screen and (max-device-width: 800px)" href="mobile.css" />
-        <title>Co//ab Entrepreneurial Workspace</title>
-    </head>
-	
-	<h1><center>co//ab</center></h1>
-	
+<head>
+	<link rel="stylesheet" type="text/css" href="main.css">
+	<link rel="stylesheet" type="text/css" media="only screen and (max-width: 480px), only screen and (max-device-width: 800px)" href="mobile.css" />
+	<title>Co//ab Entrepreneurial Workspace</title>
+</head>
+
+
 	<body>
-		<nav>
-			<ul>
-				<li><a href="home.html">Home</a></li>
-				<li><a href="moreinformation.html">More Information</a></li>
-				<li><a href="calendar.html">Calendar</a></li>
-			</ul>
-		</nav>
+	<?php include 'siteincludes/header.php';?>
+ 
+	<!--end head-->
+	
+  
+	<!--body-->
+	<?php include 'siteincludes/bodystart.php';?>
 		
 		<h2>Facilities</h2>
 		<p>Co//ab has many facilities that are available for students and entrepreneurs to reserve. Co//ab has 4 types of rooms: the large classroom space, a social space at the front, and a small "clinic" space that can be used for sessions with one of Co//ab's many mentors.</p>
 		<p>Amenities of Co//ab include coffee available around the clock, the first North State fiber Internet business customer in downtown Greensboro, and mentoring from our professionals. Currently, we have three lawyers on our staff who are available to counsel early stage entrepreneurs in the steps necessary to get their new company off the ground.</p>
 		
-		<img src="IMG_1070.JPG" alt="Large Classroom Workspace" style="width:128px;height:128px">
+		<img src="http://localhost/dir/TeamRocket/collab/media/IMG_1070.JPG" alt="Large Classroom Workspace" style="width:128px;height:128px">
 		<p>Large Classroom Workspace</p>
 		
-		<img src="IMG_1071.JPG" alt="Large Classroom Workspace" style="width:128px;height:128px">
+		<img src="http://localhost/dir/TeamRocket/collab/media/IMG_1071.JPG" alt="Large Classroom Workspace" style="width:128px;height:128px">
 		<p>"Clinic" Space</p>
 		
-		<img src="IMG_1073.JPG" alt="Large Classroom Workspace" style="width:128px;height:128px">
+		<img src="http://localhost/dir/TeamRocket/collab/media/IMG_1073.JPG" alt="Large Classroom Workspace" style="width:128px;height:128px">
 		<p>"Desks</p>
 		
+			<?php include 'siteincludes/bodyend.php';?>
+	
+		<!--end body-->
+
+	<?php include 'siteincludes/footer.php';?>
 		
 	</body>
 </html>
--- collab/siteincludes/header.php
+++ collab/siteincludes/header.php
@@ -6,6 +6,7 @@
 </head>
 
 
+
 	<table id="head">
 		<tr><td>
 			<center><h1 id="title">co//ab</h1></center>
@@ -13,7 +14,30 @@
 				<li><a id="head" href="http://localhost/dir/TeamRocket/collab/home.php">Home</a></li>
 				<li><a id="head" href="http://localhost/dir/TeamRocket/collab/moreinformation.php">More Information</a></li>
 				<li><a id="head" href="http://localhost/dir/TeamRocket/collab/calendar.php">Calendar</a></li>
+			
+
+<?PHP
+require_once("./include/membersite_config.php");
+
+if(!$fgmembersite->CheckLogin())
+{
+    echo "<li id='userInfo'><a id='userInfo' href='http://localhost/dir/TeamRocket/collab/login.php'> login </a></li>";
+    
+}
+else{
+
+	echo "<li id='userInfo'> Hello, " . $fgmembersite->UserFullName() .  "<a id='userInfo' href='http://localhost/dir/TeamRocket/collab/logout.php'> logout </a></li>";
+}
+
+?>
+
+
+
 			</ul>
+
+
+
+
 		</td></tr>
 	</table>
 	<!--end head-->
