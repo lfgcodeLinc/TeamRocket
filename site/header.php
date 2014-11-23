921a8f3dafba9ec3e0b764335558f7cf9cd0bbf7
anmart <andrewrmartinek@gmail.com>
2014-11-23 03:48:31 -0500

    HTML/CSS/PHP Mod
    Updated main page to php, moved header to separate file

---
+++ site/header.php
@@ -0,0 +1,20 @@
+	<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="main.css">
+
+</head>
+
+<?php include '../collab/login.php';?>
+	<table id="head">
+		<tr><td>
+			<center><h1 id="title">co//ab</h1></center>
+			<ul>
+				<li><a id="head" href="home.html">Home</a></li>
+				<li><a id="head" href="moreinformation.html">More Information</a></li>
+				<li><a id="head" href="calendar.html">Calendar</a></li>
+			</ul>
+		</td></tr>
+	</table>
+	<!--end head-->
+	<br/><br/><br/>
